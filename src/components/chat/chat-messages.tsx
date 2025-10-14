a4e3e4259f2e06d5401735178c3e787efd072505
YuvanX <abhivigneshofficial@gmail.com>
2025-10-14 09:41:29 +0530

    fix: chat-messages

--- src/components/chat/chat-messages.tsx
+++ src/components/chat/chat-messages.tsx
@@ -1,17 +1,24 @@
 "use client";
 
-import { MessageContext } from "@/context/MessageContext"
-import { useContext } from "react"
+import { MessageContext } from "@/context/MessageContext";
+import { useContext } from "react";
 
 export const ChatMessages = () => {
+  const { messages } = useContext(MessageContext);
 
-    const { messages } = useContext(MessageContext)
-
-    return (
-        <div className="max-w-4xl mx-auto py-10">
-            {messages.length > 0 && messages.map((message) => (
-                <div className={`${message.role === "ai" ? "bg-blue-500" : "bg-lime-500"}`}>{message.message}</div>
-            ))}
-        </div>
-    )
-}
+  return (
+    <div className="mx-auto max-w-4xl py-10">
+      {messages.length > 0 &&
+        messages.map((message) => (
+          <div key={message.message} className={`${message.role === "ai" ? "flex justify-start" : "flex justify-end"}`}>
+            <div
+              
+              className={`${message.role === "ai" ? "w-full" : "max-w-sm rounded-xl bg-stone-200 px-3 py-2 dark:bg-[#303030]"} mb-10`}
+            >
+              {message.message}
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+};
