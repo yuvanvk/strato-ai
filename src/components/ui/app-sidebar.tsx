817d5380ace5a6dd7f9c7e480024e37854a36e7f
yuvanvk <abhivigneshofficial@gmail.com>
2025-11-06 19:43:37 +0530

    feat: add command-search

--- src/components/ui/app-sidebar.tsx
+++ src/components/ui/app-sidebar.tsx
@@ -30,13 +30,21 @@ import {
 } from "./dropdown-menu";
 
 import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
-import { Badge } from "./badge";
-import { useTheme } from "next-themes";
 import axios from "axios";
 import { useEffect, useRef, useState } from "react";
 import { cn } from "@/lib/utils";
 
 import { BsThreeDots } from "react-icons/bs";
+
+import { Badge } from "./badge";
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "./command";
 
 interface Chat {
   id: string;
@@ -48,13 +56,13 @@ export const AppSidebar = () => {
   const [chats, setChats] = useState<Chat[] | []>([]);
   const [editingChatId, setEditingChatId] = useState<string | null>(null);
   const [rename, setRename] = useState<string>("");
+  const [isCommandOpen, setIsCommandOpen] = useState(false);
+
   const { open } = useSidebar();
-
-  const inputRef = useRef<HTMLInputElement | null>(null);
-
-  const router = useRouter();
   const { data: session } = authClient.useSession();
 
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const router = useRouter();
   const params = useParams<{ id: string }>();
 
   const createNewChat = async () => {
@@ -119,155 +127,184 @@ export const AppSidebar = () => {
     getAllUserChats();
   }, []);
 
-  return (
-    <Sidebar variant="inset" collapsible="icon">
-      <SidebarHeader className="space-y-2">
-        <div className="flex items-center justify-between gap-1">
-          <img src={`/logo.svg`} className="h-8 w-8" />
-          {open && <SidebarTrigger />}
-        </div>
 
-        <Button
-          onClick={createNewChat}
-          className="cursor-pointer rounded-[10px] bg-gradient-to-b from-[#5728f4] to-[#5100FF] text-[15px] text-white [box-shadow:0px_-2px_0px_-0px_#2c04b1_inset] hover:opacity-90"
-        >
-          {open ? "New Chat" : <Plus size={20} />}
-        </Button>
-      </SidebarHeader>
-      <SidebarContent>
-        {open && (
-          <div>
-            {chats.length > 0 ? (
-              <div className="flex flex-col space-y-0.5 px-1.5 py-5">
-                {chats.map((chat) => (
-                  <div
-                    onClick={() => router.push(`/chat/${chat.id}`)}
-                    key={chat.id}
-                    className={cn(
-                      "group/chat flex cursor-pointer items-center justify-between rounded-lg px-2 py-2 text-sm font-normal text-black transition hover:bg-neutral-200 dark:text-white dark:hover:bg-neutral-800",
-                      params.id === chat.id &&
-                        "bg-neutral-200 text-black dark:bg-neutral-800",
-                    )}
-                  >
-                    {editingChatId === chat.id ? (
-                      <input
-                        ref={inputRef}
-                        className="ring-offset-0 outline-none"
-                        value={rename}
-                        onKeyDown={(e) => {
-                          if (e.key === "Enter") handleRenameSubmit(chat.id);
-                          if (e.key === "Escape") cancelRename();
-                        }}
-                        onChange={(e) => setRename(e.target.value)}
-                        autoFocus
-                      />
-                    ) : (
-                      <span>{chat.title}</span>
-                    )}
-                    <span className="opacity-0 group-hover/chat:opacity-100">
-                      <DropdownMenu>
-                        <DropdownMenuTrigger className="outline-none">
-                          <BsThreeDots />
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent className="">
-                          <DropdownMenuItem>
-                            <div
-                              onClick={() => handleEdit(chat.id, chat.title)}
-                              className="flex items-center gap-1.5"
-                            >
-                              <Pencil />
-                              Rename
-                            </div>
-                          </DropdownMenuItem>
-                          <DropdownMenuSeparator />
-                          <DropdownMenuItem
-                            onClick={() => handleDelete(chat.id)}
-                          >
-                            <Trash2 className="text-red-500" />
-                            Delete
-                          </DropdownMenuItem>
-                        </DropdownMenuContent>
-                      </DropdownMenu>
-                    </span>
-                  </div>
-                ))}
-              </div>
-            ) : (
-              <span className="text-muted-foreground px-3 py-2">
-                {open && "No chats"}
-              </span>
-            )}
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if(e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault()
+        setIsCommandOpen(c => !c)
+      }
+    }
+
+    document.addEventListener("keydown", down)
+    return () => document.removeEventListener("keydown", down)
+  }, [])
+
+  return (
+    <>
+      <Sidebar variant="inset" collapsible="icon">
+        <SidebarHeader className="space-y-2">
+          <div className="flex items-center justify-between gap-1">
+            <img src={`/logo.svg`} className="h-8 w-8" />
+            {open && <SidebarTrigger />}
           </div>
-        )}
-        {!open && (
-          <div className="flex justify-center mt-1">
-            <div className="rounded-full bg-neutral-800 w-10 h-10 flex items-center justify-center">
-            <Search size={15} />
-          </div>
-          </div>
-        )}
-      </SidebarContent>
-      <SidebarFooter>
-        {!session && (
-          <button
-            onClick={() => router.push("/auth")}
-            className="flex cursor-pointer items-center justify-center gap-2 rounded-xl bg-[#101010] py-2 font-mono text-sm font-medium text-white dark:bg-white dark:text-black"
+
+          <Button
+            onClick={createNewChat}
+            className="cursor-pointer rounded-[10px] bg-gradient-to-b from-[#5728f4] to-[#5100FF] text-[15px] text-white [box-shadow:0px_-2px_0px_-0px_#2c04b1_inset] hover:opacity-90"
           >
-            <LogIn size={18} />
-            {open && "Login"}
-          </button>
-        )}
-        {session && (
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <button className="flex cursor-pointer items-center justify-between gap-x-2 rounded-xl px-1 py-1">
-                <div className="flex items-center gap-2">
-                  <Avatar className="h-8 w-8">
-                    <AvatarImage
-                      src={session.user.image!}
-                      alt={session.user.name}
-                    />
-                    <AvatarFallback>{session.user.name}</AvatarFallback>
-                  </Avatar>
-                  {open && (
-                    <div className="flex flex-col items-start">
-                      <span className="text-sm font-normal">
-                        {session.user.name}
-                      </span>
-                      <span className="text-muted-foreground text-xs capitalize">
-                        free
+            {open ? "New Chat" : <Plus size={20} />}
+          </Button>
+        </SidebarHeader>
+        <SidebarContent>
+          {open && (
+            <div>
+              {chats.length > 0 ? (
+                <div className="flex flex-col space-y-0.5 px-1.5 py-5">
+                  {chats.map((chat) => (
+                    <div
+                      onClick={() => router.push(`/chat/${chat.id}`)}
+                      key={chat.id}
+                      className={cn(
+                        "group/chat flex cursor-pointer items-center justify-between rounded-lg px-2 py-2 text-sm font-normal text-black transition hover:bg-neutral-200 dark:text-white dark:hover:bg-neutral-800",
+                        params.id === chat.id &&
+                          "bg-neutral-200 text-black dark:bg-neutral-800",
+                      )}
+                    >
+                      {editingChatId === chat.id ? (
+                        <input
+                          ref={inputRef}
+                          className="ring-offset-0 outline-none"
+                          value={rename}
+                          onKeyDown={(e) => {
+                            if (e.key === "Enter") handleRenameSubmit(chat.id);
+                            if (e.key === "Escape") cancelRename();
+                          }}
+                          onChange={(e) => setRename(e.target.value)}
+                          autoFocus
+                        />
+                      ) : (
+                        <span>{chat.title}</span>
+                      )}
+                      <span className="opacity-0 group-hover/chat:opacity-100">
+                        <DropdownMenu>
+                          <DropdownMenuTrigger className="outline-none">
+                            <BsThreeDots />
+                          </DropdownMenuTrigger>
+                          <DropdownMenuContent className="">
+                            <DropdownMenuItem>
+                              <div
+                                onClick={() => handleEdit(chat.id, chat.title)}
+                                className="flex items-center gap-1.5"
+                              >
+                                <Pencil />
+                                Rename
+                              </div>
+                            </DropdownMenuItem>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem
+                              onClick={() => handleDelete(chat.id)}
+                            >
+                              <Trash2 className="text-red-500" />
+                              Delete
+                            </DropdownMenuItem>
+                          </DropdownMenuContent>
+                        </DropdownMenu>
                       </span>
                     </div>
-                  )}
+                  ))}
                 </div>
-                {open && (
-                  <Badge variant={"outline"} className="rounded-full">
-                    Upgrade
-                  </Badge>
-                )}
-              </button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="rounded-xl">
-              <DropdownMenuItem disabled className="cursor-pointer">
-                <CircleUser />
-                {session.user.email}
-              </DropdownMenuItem>
-              <DropdownMenuItem className="cursor-pointer">
-                <Sparkle />
-                Upgrade
-              </DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem
-                className="cursor-pointer"
-                onClick={async () => await signOut()}
-              >
-                <LogOut />
-                Log out
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        )}
-      </SidebarFooter>
-    </Sidebar>
+              ) : (
+                <span className="text-muted-foreground px-3 py-2">
+                  {open && "No chats"}
+                </span>
+              )}
+            </div>
+          )}
+          {!open && (
+            <div className="mt-1 flex justify-center">
+              <div className="flex h-10 w-10 items-center justify-center rounded-full bg-neutral-100 dark:bg-neutral-800">
+                <Search onClick={() => setIsCommandOpen((c) => !c)} size={15} />
+              </div>
+            </div>
+          )}
+        </SidebarContent>
+        <SidebarFooter>
+          {!session && (
+            <button
+              onClick={() => router.push("/auth")}
+              className="flex cursor-pointer items-center justify-center gap-2 rounded-xl bg-[#101010] py-2 font-mono text-sm font-medium text-white dark:bg-white dark:text-black"
+            >
+              <LogIn size={18} />
+              {open && "Login"}
+            </button>
+          )}
+          {session && (
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <button className="flex cursor-pointer items-center justify-between gap-x-2 rounded-xl px-1 py-1">
+                  <div className="flex items-center gap-2">
+                    <Avatar className="h-8 w-8">
+                      <AvatarImage
+                        src={session.user.image!}
+                        alt={session.user.name}
+                      />
+                      <AvatarFallback>{session.user.name}</AvatarFallback>
+                    </Avatar>
+                    {open && (
+                      <div className="flex flex-col items-start">
+                        <span className="text-sm font-normal">
+                          {session.user.name}
+                        </span>
+                        <span className="text-muted-foreground text-xs capitalize">
+                          free
+                        </span>
+                      </div>
+                    )}
+                  </div>
+                  {open && (
+                    <Badge variant={"outline"} className="rounded-full">
+                      Upgrade
+                    </Badge>
+                  )}
+                </button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="rounded-xl">
+                <DropdownMenuItem disabled className="cursor-pointer">
+                  <CircleUser />
+                  {session.user.email}
+                </DropdownMenuItem>
+                <DropdownMenuItem className="cursor-pointer">
+                  <Sparkle />
+                  Upgrade
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem
+                  className="cursor-pointer"
+                  onClick={async () => await signOut()}
+                >
+                  <LogOut />
+                  Log out
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          )}
+        </SidebarFooter>
+      </Sidebar>
+
+      <CommandDialog open={isCommandOpen} onOpenChange={setIsCommandOpen} className="rounded-xl z-50">
+      <CommandInput placeholder="Type a command or search..." />
+      <CommandList>
+        <CommandEmpty>No results found.</CommandEmpty>
+        <CommandGroup heading="Suggestions">
+          {chats.map((chat) => (
+            <CommandItem onSelect={() => {
+              router.push(`/chat/${chat.id}`)
+            }} className="rounded-lg cursor-pointer" key={chat.id}>{chat.title}</CommandItem>
+          ))}
+        </CommandGroup>
+      </CommandList>
+    </CommandDialog>
+    </>
   );
 };
