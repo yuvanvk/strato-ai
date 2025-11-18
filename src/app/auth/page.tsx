6e4cf0df3b103d06bf9be69b1b823cf5c9c39825
yuvanvk <abhivigneshofficial@gmail.com>
2025-11-18 20:27:44 +0530

    fix: pre_render auth page issue

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,16 +1,9 @@
-"use client";
-
 import { Auth } from "@/components/ui/auth";
-import { authClient } from "@/lib/auth-client";
-import { redirect } from "next/navigation";
 
-
-export default async function AuthPage() {
-
-    const session = await authClient.getSession()
-    if(session) redirect("/chat")
-        
-    return <>
-        <Auth />
+export default function AuthPage() {
+  return (
+    <>
+      <Auth />
     </>
+  );
 }
--- src/components/ui/auth.tsx
+++ src/components/ui/auth.tsx
@@ -1,30 +1,47 @@
 "use client";
 
-import { signIn } from "@/lib/auth-client";
+import { authClient, signIn } from "@/lib/auth-client";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 import { FcGoogle } from "react-icons/fc";
 import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
 
 export const Auth = () => {
-
     const router = useRouter();
+    const session = authClient.useSession();
+
+    useEffect(() => {
+        if (session) {
+            router.push("/chat");
+        }
+    }, [session, router])
 
     const handleLogin = async () => {
-        await signIn()
-    }
-    return <div className="h-screen flex items-center justify-center bg-white dark:bg-[#101010] font-mono">
-        <div className="flex flex-col items-center space-y-4">
-                <img src="/logo.svg" className="w-10 h-10"/>
-                <div className="text-2xl font-medium tracking-tight ">Welcome to Strato AI</div>
-                <button onClick={handleLogin} className="bg-gradient-to-b from-[#5728f4] to-[#5100FF] hover:opacity-90 text-white min-w-sm py-3 [box-shadow:0px_-2px_0px_-0px_#2c04b1_inset] rounded-[15px] flex items-center gap-2 font-medium justify-center cursor-pointer">
-                    <FcGoogle size={25}/>
+        await signIn();
+    };
+    return (
+        <div className="flex h-screen items-center justify-center bg-white font-mono dark:bg-[#101010]">
+            <div className="flex flex-col items-center space-y-4">
+                <img src="/logo.svg" className="h-10 w-10" />
+                <div className="text-2xl font-medium tracking-tight">
+                    Welcome to Strato AI
+                </div>
+                <button
+                    onClick={handleLogin}
+                    className="flex min-w-sm cursor-pointer items-center justify-center gap-2 rounded-[15px] bg-gradient-to-b from-[#5728f4] to-[#5100FF] py-3 font-medium text-white [box-shadow:0px_-2px_0px_-0px_#2c04b1_inset] hover:opacity-90"
+                >
+                    <FcGoogle size={25} />
                     Continue with Google
                 </button>
-                <button onClick={() => router.push("/chat")} className="min-w-sm rounded-[15px] py-3 bg-[#101010] dark:bg-white text-white dark:text-black flex items-center justify-center gap-2 cursor-pointer">
-                    <MdOutlineKeyboardArrowLeft size={20}/>
+                <button
+                    onClick={() => router.push("/chat")}
+                    className="flex min-w-sm cursor-pointer items-center justify-center gap-2 rounded-[15px] bg-[#101010] py-3 text-white dark:bg-white dark:text-black"
+                >
+                    <MdOutlineKeyboardArrowLeft size={20} />
                     Back to home
                 </button>
+            </div>
         </div>
-    </div>
-}
+    );
+};
