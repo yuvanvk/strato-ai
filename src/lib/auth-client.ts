199eaecdfc6970b674ad346beee5d50646ebbee3
yuvanvk <abhivigneshofficial@gmail.com>
2025-11-18 18:58:07 +0530

    fix: polar-access-token

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,26 +1,28 @@
-import { polarClient } from "@polar-sh/better-auth";
 import { createAuthClient } from "better-auth/react";
 import { redirect } from "next/navigation";
-
+import { polarClient } from "@polar-sh/better-auth"; 
 
 
 export const authClient = createAuthClient({
-  baseURL: process.env.BETTER_AUTH_URL,
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
   plugins: [polarClient()]
 });
 
 export const signIn = async () => {
-  const data = await authClient.signIn.social({
+  await authClient.signIn.social({
     provider: "google",
     callbackURL: "/chat"
   });
 }
 
 export const signOut = async () => {
-  const data = await authClient.signOut();
+  await authClient.signOut();
   redirect("/chat")
 } 
 
 export const checkOut = async () => {
-  const res = await authClient.checkout()
+  
+  const res = await authClient.checkout({
+    slug: "pro"
+  });
 }
