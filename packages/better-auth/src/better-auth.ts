bfdb4fbb5cdf283ac588d814c6bc287e4e653bea
yuvanvk <abhivigneshofficial@gmail.com>
2026-05-11 18:24:59 +0530

    fix: baseUrl issue and trustedOrigins config

--- packages/better-auth/src/better-auth.ts
+++ packages/better-auth/src/better-auth.ts
@@ -1,15 +1,27 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "@better-auth/drizzle-adapter";
 import { db } from "@workspace/db";
+import { sendEmail } from "./send-email";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg"
   }),
   baseURL: process.env.BETTER_AUTH_URL,
+  basePath: "/api/v1/auth",
+  trustedOrigins: ["http://localhost:3000"],
   emailAndPassword: {
     enabled: true,
     requireEmailVerification: true
+  },
+  emailVerification: {
+    sendVerificationEmail: async( { user, url, token }, request) => {
+      void sendEmail({
+        to: user.email,
+        subject: "Verify your email address",
+        text: `Click the link to verify your email: ${url}`,
+      })
+    }
   },
   socialProviders: {
     google: {
