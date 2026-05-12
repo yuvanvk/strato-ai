d90003436b0f19a1b8bf589bc0865babe62f349f
yuvanvk <abhivigneshofficial@gmail.com>
2026-05-12 19:19:48 +0530

    fix: remove modular level instance and config according to worker env

--- packages/better-auth/src/better-auth.ts
+++ packages/better-auth/src/better-auth.ts
@@ -1,32 +1,27 @@
+import { createDB } from "@workspace/db";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "@better-auth/drizzle-adapter";
-import { db } from "@workspace/db";
-import { sendEmail } from "./send-email";
+import { betterAuthOptions } from "./better-auth-options";
 
-export const auth = betterAuth({
-  database: drizzleAdapter(db, {
-    provider: "pg"
-  }),
-  baseURL: process.env.BETTER_AUTH_URL,
-  basePath: "/api/v1/auth",
-  trustedOrigins: ["http://localhost:3000"],
-  emailAndPassword: {
-    enabled: true,
-    requireEmailVerification: true
-  },
-  emailVerification: {
-    sendVerificationEmail: async( { user, url, token }, request) => {
-      void sendEmail({
-        to: user.email,
-        subject: "Verify your email address",
-        text: `Click the link to verify your email: ${url}`,
-      })
-    }
-  },
-  socialProviders: {
-    google: {
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+interface CloudflareBindings {
+  DATABASE_URL: string;
+  BETTER_AUTH_SECRET: string;
+  BETTER_AUTH_URL: string;
+}
+
+export const auth = (env: CloudflareBindings) => {
+  const db = createDB(env.DATABASE_URL)
+  
+  return betterAuth({
+    ...betterAuthOptions,
+    database: drizzleAdapter(db, { provider: "pg" }),
+    baseURL: env.BETTER_AUTH_URL,
+    secret: env.BETTER_AUTH_SECRET,
+    socialProviders: {
+      google: {
+        clientId: process.env.GOOGLE_CLIENT_ID as string,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      },
     },
-  },
-});
+  });
+};
