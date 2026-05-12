1d2224c9bbc71f90c7f4739bfd2e61550e3d2e19
yuvanvk <abhivigneshofficial@gmail.com>
2026-05-12 19:21:54 +0530

    fix: export fresh db instance factory for worker env

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -2,10 +2,12 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./db/schema.js";
 
-const connectionString = process.env.DATABASE_URL!
+export const db = drizzle<typeof schema>(postgres(process.env.DATABASE_URL!, { prepare: false, max: 1 }))
 
-const client = postgres(connectionString, { prepare: false });
-export const db = drizzle<typeof schema>(client, { schema });
+export const createDB = (connectionString: string) => {
+    const client = postgres(connectionString, { prepare: false, max: 1 })
+    return drizzle(client, { schema })
+}
 
 export * from "./db/schema.js"
 export type Schema = typeof schema;
