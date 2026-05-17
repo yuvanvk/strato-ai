1952357db1fc276872ac6fe6e04c16efc48d888e
yuvanvk <abhivigneshofficial@gmail.com>
2026-05-17 09:03:10 +0530

    fix: types

--- apps/api/src/routes/auth.ts
+++ apps/api/src/routes/auth.ts
@@ -1,7 +1,8 @@
 import { Hono } from "hono";
 import { auth } from "@workspace/better-auth/server";
+import { Bindings } from "@/types";
 
-const router = new Hono<{ Bindings: CloudflareBindings }>({ strict: false });
+const router = new Hono<{ Bindings: Bindings }>({ strict: false });
 
 router.on(["POST", "GET", "OPTIONS"], '/*', (c) => {
     return auth(c.env).handler(c.req.raw)
